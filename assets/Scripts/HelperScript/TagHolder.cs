96f24d02beb918f0a5e09295a297c2903d767892
gordenfl <gordenfl@gmail.com>
2020-10-31 01:42:54 -0700

    Weapon Handler

--- assets/Scripts/HelperScript/TagHolder.cs
+++ assets/Scripts/HelperScript/TagHolder.cs
@@ -14,3 +14,16 @@ public class MouseAxis
     public const string MOUSE_X = "Mouse X";
     public const string MOUSE_Y = "Mouse Y";
 }
+
+public class AnimationTags
+{
+    public const string SHOOT_TRIGGER = "Shoot";
+    public const string AIM_TRIGGER = "Aim";
+    public const string ZOOMIN_TRIGGER = "ZoomIn";
+    public const string ZOOMOUT_TRIGGER = "ZoomOut";
+
+    public const string WALK_TRIGGER = "Walk";
+    public const string RUN_TRIGGER = "Run";
+    public const string ATTACK_TRIGGER = "Attack";
+    public const string DEAD_TRIGGER = "Dead";
+}
---
+++ assets/Scripts/Weapon Scripts/WeaponHandler.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim
+{
+    NONE,
+    SELF_AIM,
+    AIM
+}
+
+public enum WeaponFireType
+{
+    SINGLE,
+    MULTIPLE
+}
+
+public enum WeaponBulletType
+{
+    BULLET,
+    ARROW,
+    SPEAR,
+    NONE
+}
+
+public class WeaponHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+    [SerializeField]
+    public WeaponAim weapon_aim;
+    [SerializeField]
+    private GameObject muzzleFlash;
+    [SerializeField]
+    private AudioSource shootSound, reloadSound;
+
+    [SerializeField]
+    public WeaponFireType fire_type;
+
+    [SerializeField]
+    public WeaponBulletType bullet_type;
+    [SerializeField]
+    public GameObject attack_point;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    public void ShootAnimation()
+    {
+        anim.SetTrigger(AnimationTags.SHOOT_TRIGGER);
+    }
+
+    public void Aim(bool canAim)
+    {
+        anim.SetBool(AnimationTags.AIM_TRIGGER, canAim);
+    }
+
+    void Turn_On_MuzzleFlash()
+    {
+        muzzleFlash.SetActive(true);
+    }
+    void Turn_Off_MuzzleFlash()
+    {
+        muzzleFlash.SetActive(false);
+    }
+
+    void Play_ShootSound()
+    {
+        shootSound.Play();
+    }
+
+    void Play_ReloadSound()
+    {
+        reloadSound.Play();
+    }
+
+    void Turn_On_AttackPoint()
+    {
+        attack_point.SetActive(true);
+    }
+    void Turn_Off_AttackPoint()
+    {
+        if (attack_point.activeInHierarchy)
+        {
+            attack_point.SetActive(false);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
