b2dff3d72eb78856b6284e80546a33e1c396c551
gordenfl <gordenfl@gmail.com>
2020-10-31 02:02:16 -0700

    weapon load and change

--- assets/Scripts/Weapon Scripts/WeaponManager.cs
+++ assets/Scripts/Weapon Scripts/WeaponManager.cs
@@ -6,15 +6,52 @@ public class WeaponManager : MonoBehaviour
 {
     [SerializeField]
     private WeaponHandler[] weapons;
+
+    private int curWeaponIdx;
     // Start is called before the first frame update
     void Start()
     {
-
+        curWeaponIdx = 0;
+        weapons[curWeaponIdx].gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TurnOnSelectedWeapon(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            TurnOnSelectedWeapon(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            TurnOnSelectedWeapon(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            TurnOnSelectedWeapon(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            TurnOnSelectedWeapon(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            TurnOnSelectedWeapon(5);
+        }
+    } // update
 
+    void TurnOnSelectedWeapon(int weaponIdx)
+    {
+        weapons[curWeaponIdx].gameObject.SetActive(false);
+        weapons[weaponIdx].gameObject.SetActive(true);
+        curWeaponIdx = weaponIdx;
+    }
+
+    public WeaponHandler GetCurrentSelectedWeapon() {
+        return weapons[curWeaponIdx];
     }
 } // class
