62195466e755e07a76c5fd95dc53f2720058f654
gordenfl <gordenfl@gmail.com>
2021-01-02 02:11:39 -0800

    add add attack layer for boar and Cannibal

---
+++ assets/Scripts/Player Scripts/AttackScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float damage = 2.0f;
+    public float radius = 1.0f;
+    public LayerMask  layerMask;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, layerMask);
+        if(hits.Length>0) {
+            print("we hit:"+hits[0].gameObject.tag);
+            gameObject.SetActive(false);
+        }
+    }
+}
